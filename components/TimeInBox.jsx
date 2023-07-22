1381a53422c5b5912f8459be1fdb98aaad859ee7
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-07-22 13:30:27 +0000

    Setup Project Structure

---
+++ components/TimeInBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TimeInBox = () => {
+  return (
+    <>
+    <h1 className='text-4xl'>SVL Sign System</h1>
+    <div  className='w-96 h-96 border-2 border-slate-800 rounded'>
+        TimeInBox     
+    </div>
+    </>
+  )
+}
+
+export default TimeInBox
