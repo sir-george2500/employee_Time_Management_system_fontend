f632ee9caf966d47f3eb17d24e6e1a09410173da
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-07-22 13:52:31 +0000

    added the logo

--- components/TimeInBox.jsx
+++ components/TimeInBox.jsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 const TimeInBox = () => {
   return (
     <>
-    <h1 className='text-4xl'>SVL Sign System</h1>
+    <div  className='flex'>
+   <Image 
+     src={require("../public/image/svl_logo.png")}
+     width={100}
+     height={100}
+     className='flex-initial mr-4'
+   />
+    <h1 className='flex-1 text-3xl pt-9'>Sign System</h1>
+    </div>
     <div  className='w-96 h-96 border-2 border-slate-800 rounded'>
         TimeInBox     
     </div>
