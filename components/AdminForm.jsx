d9e05c1f9e77081247c92e596f3c23592635e07e
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-04 16:06:57 +0000

    added the login Button the admin form

--- components/AdminForm.jsx
+++ components/AdminForm.jsx
@@ -6,22 +6,33 @@ import React from 'react'
 const AdminForm = ({name, text, value, errorMessage, onChange,formik }) => {
   return (
     <>
+    <h1>Login as Admin</h1>
+    <div>
     <TextInput
     name={name}
-    placeholder='Please enter your name'
+    label={"username *"}
+    placeholder='Please enter your username'
     leftIcon={mdiAccount}
     value={value}
     onChange={onChange}
 
     />
+
     <TextInput
     name={name}
+    label={"Password *"}
     placeholder='Please Enter your password'
     leftIcon={mdiLock}
     rightIcon={mdiEye}
     value={value}
     onChange={onChange}
-    />
+    /> 
+    <div class="flex mt-4 justify-center">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-40">Login</button>
+    </div>
+
+
+    </div>
     </>
   )
 }
