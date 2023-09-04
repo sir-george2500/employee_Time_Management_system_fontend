e77fc144cd2965d3b83f8cffa8da16aa2dde31a9
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-04 16:17:12 +0000

    added the type for password

--- components/AdminForm.jsx
+++ components/AdminForm.jsx
@@ -26,6 +26,7 @@ const AdminForm = ({name, text, value, errorMessage, onChange,formik }) => {
     rightIcon={mdiEye}
     value={value}
     onChange={onChange}
+    type="password"
     /> 
     <div class="flex mt-4 justify-center">
             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-40">Login</button>
