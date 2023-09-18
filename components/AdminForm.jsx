510a3ab87de9659669070fa6e63ac37da11f0332
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-18 17:24:01 +0000

    added feature for password visible and invisiable

--- components/AdminForm.jsx
+++ components/AdminForm.jsx
@@ -1,9 +1,17 @@
+"use client"
 import TextInput from './TextInput';
-import { mdiAccount , mdiEye, mdiLock } from '@mdi/js';
+import { mdiAccount , mdiEye, mdiEyeOff, mdiLock } from '@mdi/js';
 import Icon from '@mdi/react';
-import React from 'react'
+import React, { useState }from 'react'
 
 const AdminForm = ({name, text, value, errorMessage, onChange,formik }) => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  
   return (
     <>
     <h1 className='mb-4 font-bold'>Login as Admin</h1>
@@ -18,16 +26,17 @@ const AdminForm = ({name, text, value, errorMessage, onChange,formik }) => {
 
     />
 
-    <TextInput
-    name={name}
-    label={"Password *"}
-    placeholder='password'
-    leftIcon={mdiLock}
-    rightIcon={mdiEye}
-    value={value}
-    onChange={onChange}
-    type="password"
-    /> 
+      <TextInput
+        name={name}
+        label={"Password *"}
+        placeholder='password'
+        leftIcon={mdiLock}
+        rightIcon={showPassword ? mdiEye : mdiEyeOff}
+        value={value}
+        onChange={onChange}
+        onPressRightIcon={togglePasswordVisibility}
+        type={showPassword ? 'text' : 'password'}
+      />
     <div class="flex mt-4 justify-center">
             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-40">Login</button>
     </div>
