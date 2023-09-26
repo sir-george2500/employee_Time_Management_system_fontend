d9d3ba417c6276fd12f560a3323543c717c2f564
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-26 15:48:30 +0000

    added improvement for the dashboard view

--- components/dashboard_components/DashboardView.jsx
+++ components/dashboard_components/DashboardView.jsx
@@ -1,9 +1,29 @@
-
 const DashboardView = () => {
-  // Add your sidebar content here
   return (
-    <div className="ml-1 mt-2 h-screen bg-stone-900 flex-1">
-      <p>Hello World</p>
+    <div className="flex-1 flex">
+      <div className="flex-1 ml-2 mt-2 h-screen bg-stone-200 rounded-lg shadow-lg p-4">
+        <h1 className="text-3xl font-bold mb-6 text-stone-900">Dashboard</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {/* Widget 1 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold mb-4">Widget 1</h2>
+            {/* Add widget content here */}
+          </div>
+
+          {/* Widget 2 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold mb-4">Widget 2</h2>
+            {/* Add widget content here */}
+          </div>
+
+          {/* Widget 3 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold mb-4">Widget 3</h2>
+            {/* Add widget content here */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
