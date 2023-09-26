0b5f9050d1695cad4c4a895fa93b8437ce610a58
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-26 15:25:22 +0000

    added improve the hover of the side bar

--- components/dashboard_components/SideBar.jsx
+++ components/dashboard_components/SideBar.jsx
@@ -7,19 +7,19 @@ const Sidebar = () => {
         <h1 className="text-white text-2xl font-bold mb-4">Dashboard</h1>
         <ul className="list-none p-0 m-0">
           <li className="mb-2">
-            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md">
+            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md hover:bg-stone-800">
               <i className="fas fa-chart-bar mr-2"></i>
               Overview
             </a>
           </li>
           <li className="mb-2">
-            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md">
+            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md hover:bg-stone-800">
               <i className="fas fa-users mr-2"></i>
               Employee
             </a>
           </li>
           <li className="mb-2">
-            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md">
+            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md hover:bg-stone-800">
               <i className="fas fa-user-graduate mr-2"></i>
               Student
             </a>
@@ -30,13 +30,13 @@ const Sidebar = () => {
       <div className="p-4 border-t border-stone-800">
         <ul className="list-none p-0 m-0">
           <li className="mb-2">
-            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md">
+            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md hover:bg-stone-800">
               <i className="far fa-calendar-alt mr-2"></i>
               Attendance
             </a>
           </li>
           <li className="mb-2">
-            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md">
+            <a href="#" className="flex items-center text-white transition-all duration-300 hover:text-blue-500 px-2 py-1 rounded-md hover:bg-stone-800">
               <i className="far fa-file-alt mr-2"></i>
               Generate Report
             </a>
